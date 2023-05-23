09e143fa7fc1a66127a7ae8ef979bfac8d0aa691
Simon Vriesema <simon.vriesema@icloud.com>
2023-05-23 15:09:24 +0200

    Opdracht versie 1 af

--- src/com/ing/zoo/Zoo.java
+++ src/com/ing/zoo/Zoo.java
@@ -1,16 +1,19 @@
 package com.ing.zoo;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Zoo {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        // Define an array of commands
         String[] commands = new String[4];
         commands[0] = "hello";
         commands[1] = "give leaves";
         commands[2] = "give meat";
         commands[3] = "perform trick";
 
+        // Create instances of animal classes
         Lion henk = new Lion();
         henk.name = "henk";
         Hippo elsa = new Hippo();
@@ -22,16 +25,49 @@ public class Zoo {
         Zebra marty = new Zebra();
         marty.name = "marty";
 
+        // Read user input
         Scanner scanner = new Scanner(System.in);
         System.out.print("Voer uw command in: ");
-
         String input = scanner.nextLine();
-        if(input.equals(commands[0] + " henk"))
-        {
+
+        // Check the input and perform actions accordingly
+        if (input.equals(commands[0])) {
+            // Execute 'hello' command for all animals
             henk.sayHello();
-        }
-        else
-        {
+            elsa.sayHello();
+            dora.sayHello();
+            wally.sayHello();
+            marty.sayHello();
+        } else if (input.equals(commands[1])) {
+            // Execute 'hello' command for all animals
+            elsa.eatLeaves();
+            dora.eatLeaves();
+            marty.eatLeaves();
+        } else if (input.equals(commands[2])) {
+            // Execute 'hello' command for all animals
+            henk.eatMeat();
+            dora.eatMeat();
+            wally.eatMeat();
+        } else if (input.equals(commands[3])) {
+            // Execute 'hello' command for all animals
+            dora.performTrick();
+            wally.performTrick();
+        } else if (input.equals(commands[0] + " henk")) {
+            // Execute 'hello' command for Lion named 'henk'
+            henk.sayHello();
+        } else if (input.equals(commands[0] + " elsa")) {
+            // Execute 'hello' command for Lion named 'henk'
+            elsa.sayHello();
+        } else if (input.equals(commands[0] + " dora")) {
+            // Execute 'hello' command for Lion named 'henk'
+            dora.sayHello();
+        } else if (input.equals(commands[0] + " wally")) {
+            // Execute 'hello' command for Lion named 'henk'
+            wally.sayHello();
+        } else if (input.equals(commands[0] + " marty")) {
+            // Execute 'hello' command for Lion named 'henk'
+            marty.sayHello();
+        } else {
             System.out.println("Unknown command: " + input);
         }
     }
